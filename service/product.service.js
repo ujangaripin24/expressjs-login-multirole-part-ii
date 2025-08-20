aa0c8e4799757533d5415c8eced1b41f9d8c4d3c
ujangaripin24 <ujangarifin06@gmail.com>
2025-08-20 09:49:49 +0700

    d

---
+++ service/product.service.js
@@ -0,0 +1,85 @@
+import db from "../models/index.js";
+import dotenv from 'dotenv'
+import { Op, where } from 'sequelize';
+dotenv.config()
+
+const { TblProduct, TblUser } = db;
+const DEFAULT_AVATAR = process.env.DEFAULT_AVATAR;
+
+export const getProduct = async (role, userId) => {
+    if (role === 'admin') {
+        return await TblProduct.findAll({
+            attributes: ['uuid', 'name', 'link_picture', 'price'],
+            include: [{ model: TblUser, attributes: ['name', 'link_picture', 'email'] }]
+        })
+    }
+    return await TblProduct.findAll({
+        where: { userId },
+        attributes: ['uuid', 'name', 'link_picture', 'price'],
+        include: [{ model: TblUser, attributes: ['name', 'link_picture', 'email'] }]
+    })
+}
+
+export const getProductById = async (uuid, role, userId) => {
+    const product = await TblProduct.findOne({ where: { uuid } })
+    if (!product) return null
+
+    if (role === 'admin') {
+        return await TblProduct.findOne({
+            attributes: ['uuid', 'name', 'link_picture', 'price'],
+            where: { uuid: product.uuid },
+            include: [{ model: TblUser, attributes: ['name', 'link_picture', 'email'] }]
+        })
+    }
+
+    return await TblProduct.findOne({
+        where: { [Op.and]: [{ uuid: product.uuid }, { userId }] },
+        attributes: ['uuid', 'name', 'link_picture', 'price'],
+        include: [{ model: TblUser, attributes: ['name', 'link_picture', 'email'] }]
+    })
+}
+
+export const createProduct = async (data) => {
+    const { name, link_picture, price, userId } = data
+    const product = await TblProduct.create({
+        name,
+        link_picture: link_picture || DEFAULT_AVATAR,
+        price,
+        userId
+    })
+    return product
+}
+
+export const updateProduct = async (uuid, role, userId, data) => {
+    const product = await TblProduct.findOne({
+        where: { uuid }
+    })
+    if (!product) throw new Error("User Tidak ada");
+
+    if (role === 'admin') {
+        await product.update(data, { where: { uuid: product.uuid } })
+    } else {
+        if (userId !== product.userId) throw new Error("Akses terlarang");
+        await product.update(data, {
+            where: { [Op.and]: [{ uuid: product.uuid }, { userId }] }
+        })
+    }
+    return product
+}
+
+export const deleteProduct = async (uuid, role, userId) => {
+    const product = await TblProduct.findOne({
+        where: { uuid }
+    })
+    if (!product) throw new Error("User Tidak ada");
+
+    if (role === 'admin') {
+        await product.destroy({ where: { uuid: product.uuid } })
+    } else {
+        if (userId !== product.userId) throw new Error("Akses terlarang")
+        await TblProduct.destroy({
+            where: { [Op.and]: [{ uuid: product.uuid }, { userId }] }
+        })
+    }
+    return product
+}
