93ce1c71fdcb589bc435c1a76b7663b5b6f5dee1
ujangaripin24 <ujangarifin06@gmail.com>
2025-08-26 15:53:46 +0700

    add pug file

--- service/sender-main.service.js
+++ service/sender-main.service.js
@@ -1,5 +1,7 @@
 import nodemailer from 'nodemailer'
 import dotenv from 'dotenv'
+import path from 'node:path';
+import pug from 'pug'
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
@@ -10,20 +12,25 @@ const transporter = nodemailer.createTransport({
     },
 })
 
-export const sendLoginAlert = async (to, name) => {
-  try {
-    await transporter.sendMail({
-      from: `"MyApp Alert" <${process.env.SMTP_USER}>`,
-      to,
-      subject: "Login Alert - MyApp",
-      text: `Halo ${name},\n\nAkun Anda baru saja login.`,
-      html: `<p>Halo <b>${name}</b>,</p>
-             <p>Akun Anda baru saja login pada aplikasi <b>MyApp</b>.</p>
-             <p>Jika ini bukan Anda, segera amankan akun Anda.</p>`,
-    });
+export const sendLoginAlert = async (to, name, email) => {
+    try {
+        const templatePath = path.resolve("views/email/loginAlert.pug")
+        const html = pug.renderFile(templatePath, {
+            name,
+            email,
+            time: new Date().toLocaleString("id-ID")
+        });
 
-    console.log("✅ Email alert terkirim ke:", to);
-  } catch (err) {
-    console.error("❌ Gagal kirim email:", err);
-  }
+        await transporter.sendMail({
+            from: `"MyApp Alert" <${process.env.SMTP_USER}>`,
+            to,
+            subject: "Login Alert - MyApp",
+            text: `Halo ${name},\n\nAkun Anda baru saja login.`,
+            html,
+        });
+
+        console.log("✅ Email alert terkirim ke:", to);
+    } catch (err) {
+        console.error("❌ Gagal kirim email:", err);
+    }
 };
