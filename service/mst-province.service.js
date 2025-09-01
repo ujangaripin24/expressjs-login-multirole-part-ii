f8e69b6fb2fa6beea10d9cd826c136b302dabd72
ujangaripin24 <ujangarifin06@gmail.com>
2025-09-01 11:43:56 +0700

    add service for province

---
+++ service/mst-province.service.js
@@ -0,0 +1,45 @@
+import db from "../models/index.js"
+import fs from "fs"
+import csv from "csv-parser"
+import { resolve } from "path";
+import { rejects } from "assert";
+const { TblMstProvince } = db;
+
+export const uploadProvinceCSV = async (filePath) => {
+  return new Promise((resolve, rejects) => {
+    const results = [];
+
+    fs.createReadStream(filePath)
+      .pipe(csv())
+      .on("data", (row) => {
+        results.push({
+          id: parseInt(row.id),
+          name_province: row.name_province,
+        });
+      })
+      .on("end", async () => {
+        try {
+          await TblMstProvince.bulkCreate(results, {
+            ignoreDuplicates: true,
+          });
+
+          resolve({
+            message: "Data provinsi berhasil diimport",
+            count: results.length,
+          })
+        } catch (error) {
+          rejects(error)
+        }
+      })
+      .on("error", (error) => {
+        rejects(error)
+      })
+  })
+}
+
+export const getAllProvinces = async () => {
+  return await TblMstProvince.findAll({
+    attributes: ["id", "name_province"],
+    order: [["name_province", "ASC"]],
+  })
+}
