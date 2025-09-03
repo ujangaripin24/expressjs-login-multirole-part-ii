d643a56907b4113a665e560e6f322102da92fa8c
ujangaripin24 <ujangarifin06@gmail.com>
2025-09-03 15:29:43 +0700

    add guard post adn update market

--- routes/market-data.routes.js
+++ routes/market-data.routes.js
@@ -2,9 +2,10 @@ import express from "express";
 import * as marketController from '../controller/market-data.controller.js'
 import { createMarketValidator, updateMarketValidator } from "../validator/market.validator.js";
 import { validationResult } from "express-validator";
+import { guardMiddleware } from "../middleware/auth.middleware.js";
 const router = express.Router()
 
-router.post('/market/data/create', createMarketValidator, (req, res, next) => {
+router.post('/market/data/create', guardMiddleware,createMarketValidator, (req, res, next) => {
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() })
@@ -14,7 +15,7 @@ router.post('/market/data/create', createMarketValidator, (req, res, next) => {
 }, marketController.createMarket)
 router.get('/market/data/get-all', marketController.getMarketData)
 router.get('/market/data/get-data-by-region', marketController.getAllMarketWithRegion)
-router.put('/market/data/update/:id', updateMarketValidator, (req, res, next) => {
+router.put('/market/data/update/:id', guardMiddleware, updateMarketValidator, (req, res, next) => {
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() })
